6f94ab5f41d7cb8adcb6b25ab110fb11d8259dab
Midolii <462885536@qq.com>
2020-04-05 18:10:31 +0800

    pop_update

---
+++ content-scripts.js
@@ -0,0 +1,8 @@
+chrome.runtime.sendMessage({ msgType: 'checkUrl', url: document.URL }, (result) => {
+    if (result == false) {
+        chrome.runtime.sendMessage({ content: 'false' }, (result) => { console.log(false) })
+    } else {
+        chrome.runtime.sendMessage({ content: 'true' }, (result) => { console.log(true) })
+    }
+
+})
