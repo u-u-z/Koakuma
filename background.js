9458f7080494b5dcf5eec02c8b818fe12b83f597
Remi_IO <i@linux.dog>
2020-04-05 16:15:54 +0800

    add switch for msg addListener.

--- background.js
+++ background.js
@@ -1,27 +1,35 @@
 const patchVideoUrl = "https://patchyvideo.com/be/getvideo_url.do"
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.url && request.msgType == "checkUrl") {
-    fetch(patchVideoUrl, {
-      body: JSON.stringify({
-        url: request.url
-      }),
-      headers: {
-        'content-type': 'application/json'
-      },
-      method: 'POST'
-    })
-      .then(response => response.json())
-      .then(data => {
-        if (data.status == "SUCCEED") {
-          sendResponse(true)
-        } else {
-          sendResponse(false)
-        }
-      })
-    return true
-  } else {
-    sendResponse(false)
-  }
 
+  switch (request.msgType) {
+    case "checkUrl":
+      if (request.url) {
+        fetch(patchVideoUrl, {
+          body: JSON.stringify({
+            url: request.url
+          }),
+          headers: {
+            'content-type': 'application/json'
+          },
+          method: 'POST'
+        })
+          .then(response => response.json())
+          .then(data => {
+            if (data.status == "SUCCEED") {
+              sendResponse(true)
+            } else {
+              sendResponse(false)
+            }
+          })
+        return true
+      } else {
+        sendResponse(false)
+      }
+      break;
+
+    default:
+      sendResponse(false)
+      break;
+  }
 });
--- content-scripts.js
+++ content-scripts.js
@@ -1,3 +1,7 @@
 chrome.runtime.sendMessage({ msgType: 'checkUrl', url: document.URL }, (result) => {
-  console.log(`小恶魔说：`,result)
+  console.log(`小恶魔说：`, result)
+  if (result) {
+    chrome.browserAction.setBadgeText({ text: 'new' });
+    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });Î
+  }
 });
