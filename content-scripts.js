134f8157409a4fbbd7400ee51ec7f2ac1a22890a
Remi_IO <i@linux.dog>
2020-04-05 19:49:20 +0800

    add setInterval

--- content-scripts.js
+++ content-scripts.js
@@ -1,8 +1,10 @@
-chrome.runtime.sendMessage({ msgType: 'checkUrl', url: document.URL }, (result) => {
-    if (result == false) {
-        chrome.runtime.sendMessage({ content: 'false' }, (result) => { console.log(false) })
-    } else {
-        chrome.runtime.sendMessage({ content: 'true' }, (result) => { console.log(true) })
-    }
+setInterval(() => {
+    chrome.runtime.sendMessage({ msgType: 'checkUrl', url: document.URL }, (result) => {
+        if (result == false) {
+            chrome.runtime.sendMessage({ content: 'false' }, (result) => { console.log(false) })
+        } else {
+            chrome.runtime.sendMessage({ content: 'true' }, (result) => { console.log(true) })
+        }
 
-})
+    })
+}, "5000");
