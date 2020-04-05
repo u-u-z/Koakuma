dbed946f0b3d823ca76588a73cf63a26025ed47b
Remi_IO <i@linux.dog>
2020-04-05 16:26:47 +0800

    add updateBadge for background.js

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
 const patchVideoUrl = "https://patchyvideo.com/be/getvideo_url.do"
 
+const updateBadge = (status) => {
+  if (status) {
+    chrome.browserAction.setBadgeText({ text: '有了' });
+    chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
+  } else {
+    chrome.browserAction.setBadgeText({ text: '没有' });
+    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+  }
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
   switch (request.msgType) {
@@ -17,8 +27,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           .then(response => response.json())
           .then(data => {
             if (data.status == "SUCCEED") {
+              updateBadge(true)
               sendResponse(true)
             } else {
+              updateBadge(false)
               sendResponse(false)
             }
           })
@@ -27,9 +39,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         sendResponse(false)
       }
       break;
+    case "updateBadge":
 
+      break;
     default:
       sendResponse(false)
       break;
   }
 });
+
